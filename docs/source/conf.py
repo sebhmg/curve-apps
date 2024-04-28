4059aef82253687e08a400115b36a732d7c18c46
Sébastien Hensgen <sebastienh@mirageoscience.com>
2024-04-28 18:32:04 -0400

    GEOPY-1472: fix mypy error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = []
+exclude_patterns: list[str] = []
 todo_include_todos = True
 
 # -- Options for auto-doc ----------------------------------------------------
