239f612151f3834809e914c24f44d40ff32116e9
benjamink <benjamink@mirageoscience.com>
2024-06-18 14:37:14 -0700

    working driver

--- curve_apps/contours/driver.py
+++ curve_apps/contours/driver.py
@@ -16,16 +16,18 @@ from __future__ import annotations
 
 import logging
 import sys
+from collections.abc import Callable
 
-import matplotlib.pyplot as plt
 import numpy as np
 from geoapps_utils.formatters import string_name
-from geoh5py.objects import Curve, Grid2D
+from geoh5py.objects import Curve
 from geoh5py.ui_json import InputFile, utils
+from scipy.interpolate import interp1d
+from skimage import measure
 
 from curve_apps.contours.params import ContourParameters
 from curve_apps.driver import BaseCurveDriver
-from curve_apps.utils import extract_data, set_vertices_height
+from curve_apps.utils import interp_to_grid, set_vertices_height
 
 logger = logging.getLogger(__name__)
 
@@ -49,60 +51,131 @@ class ContoursDriver(BaseCurveDriver):
 
         with utils.fetch_active_workspace(self.workspace, mode="r+"):
             logger.info("Generating contours ...")
-            contours = ContoursDriver.get_contours(self.params)
-            vertices, cells, values = extract_data(contours)
-            if vertices:
-                locations = np.vstack(vertices)
+            grid, data = interp_to_grid(
+                self.params.source.objects,
+                self.params.source.data.values,
+                self.params.detection.resolution,
+                self.params.detection.max_distance,
+            )
+
+            vertices, edges, values = ContoursDriver.get_contours(
+                grid, data, self.params.detection.contours
+            )
+            if len(vertices) > 0:
+                locations = vertices
                 if self.params.output.z_value:
-                    locations = np.c_[locations, np.hstack(values)]
+                    locations = np.c_[locations, values]
                 else:
                     locations = set_vertices_height(
                         locations, self.params.source.objects
                     )
+            else:
+                raise ValueError(
+                    "No contours detected. Check that the requested contour "
+                    "values are within the bounds of the data."
+                )
 
             curve = Curve.create(
                 self.workspace,
                 name=string_name(self.params.output.export_as),
                 vertices=locations,
-                cells=np.vstack(cells).astype("uint32"),
+                cells=edges,
                 parent=self.out_group,
             )
             curve.add_data(
-                {self.params.source.data.name: {"values": np.hstack(values)}}
+                {
+                    self.params.source.data.name: {
+                        "association": "VERTEX",
+                        "values": values,
+                    }
+                }
             )
 
             return curve
 
     @staticmethod
-    def get_contours(params: ContourParameters):
+    def get_contours(
+        grid: list[np.ndarray], data: np.ndarray, contour_list: list[float]
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         """
-        Calculate contour from source data.
+        Return vertices, edges, and values for contours.
 
-        :param params: Contour parameters
+        :param grid: list of x and y grids.
+        :param data: 2D array of data living in grid.
+        :param contour_list: list of contour values.
         """
 
-        entity = params.source.objects
-        data = params.source.data
-        locations = entity.locations
-        x, y = locations[:, :2].T
-        axis = plt.axes()
-        if isinstance(entity, Grid2D):
-            # TODO: Replace with scikit-image contour algorithm
-            contours = axis.contour(
-                x.reshape(entity.shape, order="F"),
-                y.reshape(entity.shape, order="F"),
-                data.values.reshape(entity.shape, order="F"),
-                levels=params.detection.contours,
-            )
-        else:
-            # TODO: Replace with scikit-image contour algorithm
-            contours = axis.tricontour(
-                x,
-                y,
-                data.values,
-                levels=params.detection.contours,
-            )
-        return contours
+        interp = ContoursDriver.image_to_grid(data, grid)
+        vertices, edges, values = [], [], []
+        v0 = 0
+        for contour in contour_list:
+
+            coords = measure.find_contours(data, contour, fully_connected="low")
+            if not coords:
+                continue
+
+            for coord in coords:
+                segment_vertices = interp(coord[:, 1], coord[:, 0])
+                nv = len(segment_vertices)
+                segment_edges = [[k, k + 1] for k in range(v0, v0 + (nv - 1))]
+                vertices.append(segment_vertices)
+                edges.append(segment_edges)
+                values.append([contour] * nv)
+                v0 += nv
+
+        return (
+            np.vstack(vertices),
+            np.vstack(edges).astype("uint32"),
+            np.hstack(values),
+        )
+
+    @staticmethod
+    def image_to_grid(image, grid) -> Callable:
+        """
+        Returns a function to interpolate from image to grid coordinates.
+
+        :param grid: list of x and y grids.
+        """
+        col = np.arange(image.shape[1])
+        row = np.arange(image.shape[0])
+        x_interp = interp1d(col, np.unique(grid[0]))
+        y_interp = interp1d(row, np.unique(grid[1]))
+
+        def interpolator(col, row):
+            return np.c_[x_interp(col), y_interp(row)]
+
+        return interpolator
+
+    # @staticmethod
+    # def get_contours(params: ContourParameters):
+    #     """
+    #     Calculate contour from source data.
+    #
+    #     :param params: Contour parameters
+    #     """
+    #
+    #     entity = params.source.objects
+    #     data = params.source.data
+    #     locations = entity.locations
+    #     x, y = locations[:, :2].T
+    #     axis = plt.axes()
+    #     if isinstance(entity, Grid2D):
+    #         # TODO: Replace with scikit-image contour algorithm
+    #         contours = axis.contour(
+    #             x.reshape(entity.shape, order="F"),
+    #             y.reshape(entity.shape, order="F"),
+    #             data.values.reshape(entity.shape, order="F"),
+    #             levels=params.detection.contours,
+    #         )
+    #     else:
+    #         # TODO: Replace with scikit-image contour algorithm
+    #         contours = axis.tricontour(
+    #             x,
+    #             y,
+    #             data.values,
+    #             levels=params.detection.contours,
+    #         )
+    #     return contours
 
 
 if __name__ == "__main__":
