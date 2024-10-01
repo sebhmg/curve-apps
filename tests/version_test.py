1670b9414c914a156a8ab46aa9eef31b4f3c130c
SophieCurinier <sophiec@mirageoscience.com>
2024-10-01 09:30:06 -0400

    [DEVOPS-504] Add deploy workflows

--- tests/version_test.py
+++ tests/version_test.py
@@ -13,13 +13,16 @@ from __future__ import annotations
 
 import re
 from pathlib import Path
+import subprocess
 
 import tomli as toml
+import yaml
+from jinja2 import Template
 
 import curve_apps
 
 
-def get_version():
+def get_pyproject_version():
     path = Path(__file__).resolve().parents[1] / "pyproject.toml"
 
     with open(str(path), encoding="utf-8") as file:
@@ -27,10 +30,35 @@ def get_version():
 
     return pyproject["tool"]["poetry"]["version"]
 
+def get_conda_recipe_version():
+    path = Path(__file__).resolve().parents[1] / "meta.yaml"
+
+    with open(str(path), encoding="utf-8") as file:
+        content = file.read()
+
+    template = Template(content)
+    rendered_yaml = template.render()
+
+    recipe = yaml.safe_load(rendered_yaml)
+
+    return recipe["package"]["version"]
+
+def get_conda_recipe_source_url():
+    path = Path(__file__).resolve().parents[1] / "meta.yaml"
+
+    with open(str(path), encoding="utf-8") as file:
+        content = file.read()
+
+    template = Template(content)
+    rendered_yaml = template.render()
+
+    recipe = yaml.safe_load(rendered_yaml)
+
+    return (recipe["source"]["url"], recipe["source"]["sha256"])
 
 def test_version_is_consistent():
-    assert curve_apps.__version__ == get_version()
-
+    assert curve_apps.__version__ == get_pyproject_version()
+    assert curve_apps.__version__ == get_conda_recipe_version()
 
 def test_version_is_semver():
     semver_re = (
@@ -40,3 +68,17 @@ def test_version_is_semver():
         r"(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
     )
     assert re.search(semver_re, curve_apps.__version__) is not None
+
+def test_conda_recipe_source_is_valid():
+    url, sha256 = get_conda_recipe_source_url()
+
+    result = subprocess.run(
+        ["curl", "-sL", url, "|", "openssl", "sha256"],
+        capture_output=True,
+        text=True,
+        shell=True
+    )
+
+    result_sha256 = result.stdout.strip().split(" ")[-1]
+
+    assert result_sha256 == sha256
